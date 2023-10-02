88c84c2d62633ece51115a38db6d678bd36c084e
Ndaa01 <ananda.idn12345@gmail.com>
2023-10-02 13:55:50 +0700

    Final Modification

--- Pemilihan2Percobaan205.java
+++ Pemilihan2Percobaan205.java
@@ -12,15 +12,15 @@ public class Pemilihan2Percobaan205 {
         System.out.println("Masukan Sudut 3: ");
         sudut3 = input05.nextFloat();
         totalSudut = sudut1+sudut2+sudut3;
-
-        if (totalSudut == 180) {
-            if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90))
+ 
+        if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90)){
                 System.out.println("Segitiga Siku-Siku");    
-            else 
-                System.out.println("Bukan Segitiga Siku-Siku");
-            }
-         else {
-            System.out.println("Bukan Segitiga");
+        }else if (sudut1 == sudut2 && sudut2 == sudut3) { 
+                System.out.println("Segitiga Sama Sisi");
+        }else if (sudut1 == sudut2 || sudut1 == sudut3 || sudut2 == sudut3) {
+                System.out.println("Segitiga Sama kaki");
+        }else {
+            System.out.println("Segitiga Sembarang");
         }
     }
 }
