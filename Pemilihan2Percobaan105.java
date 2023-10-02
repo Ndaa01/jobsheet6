2cf0e8031310fbadc78e4728dd631210f714525f
Ndaa01 <ananda.idn12345@gmail.com>
2023-10-02 12:40:57 +0700

    Modifikasi 1

--- Pemilihan2Percobaan105.java
+++ Pemilihan2Percobaan105.java
@@ -9,8 +9,11 @@ public class Pemilihan2Percobaan105 {
 
 
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) !=0)
-            System.out.println("Tahun Kabisat");
+            if ((tahun % 100) !=0 || tahun % 400 == 0) {
+                System.out.println("Tahun Kabisat");
+            } else {
+                System.out.println("Bukan Tahun Kabisat");
+            }
         }else
             System.out.println("Bukan Tahun kabisat");
     }
